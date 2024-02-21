b0e5fc8a5e124902a7a3f6ef632212d8f5019f50
Eugene600 <ugowach160@gmail.com>
2024-02-21 18:53:27 +0300

    Commented the appBar out of login page.
    The idea is to introduce it after the user logs in

--- flutter_application_2/lib/Views/login.dart
+++ flutter_application_2/lib/Views/login.dart
@@ -10,34 +10,32 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              "https://netstorage-tuko.akamaized.net/images/0fgjhs6ffkq54dn1o.jpg?imwidth=1200",
-              width: 200.0,
-              height: 100.0,
-            ),
-            // SizedBox(
-            //   width: 150,
-            // ),
-            Text(
-              "Daystar University",
-            ),
-          ],
-        ),
-        backgroundColor: primaryColor,
-        foregroundColor: appWhiteColor,
-        centerTitle: true,
-        toolbarHeight: 150,
-        actions: [
-          Icon(Icons.refresh),
-        ],
-      ),
+      // appBar: AppBar(
+      //   title: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Image.network(
+      //         "https://netstorage-tuko.akamaized.net/images/0fgjhs6ffkq54dn1o.jpg?imwidth=1200",
+      //         width: 200.0,
+      //         height: 100.0,
+      //       ),
+      //       Text(
+      //         "Daystar University",
+      //       ),
+      //     ],
+      //   ),
+      //   backgroundColor: primaryColor,
+      //   foregroundColor: appWhiteColor,
+      //   centerTitle: true,
+      //   toolbarHeight: 150,
+      //   actions: [
+      //     Icon(Icons.refresh),
+      //   ],
+      // ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(40, 80, 40, 20),
+          // padding: EdgeInsets.fromLTRB(40, 80, 40, 20),
+          padding: EdgeInsets.fromLTRB(40, 40, 40, 40),
           child: Container(
             padding: EdgeInsets.all(30),
             decoration: BoxDecoration(
